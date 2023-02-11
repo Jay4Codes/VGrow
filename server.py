afbd63b77fd4ca960940c7d99d3ddc0b82f88354
Jay Jain <jay.jain@neyx.in>
2023-02-11 20:45:10 +0530

    flask app optimized

--- server.py
+++ server.py
@@ -1,25 +1,18 @@
-from __future__ import print_function
-# from flask_cors import CORS, cross_origin
+from flask_cors import CORS
 from json import *
-# import sys
 from flask import Flask, request, jsonify
 import pandas as pd
 import numpy as np
-# import matplotlib.pyplot as plt
-# import seaborn as sns
-# from sklearn.metrics import classification_report
-# from sklearn import metrics
-# from sklearn import tree
 import pickle
 import requests
 import warnings
+import joblib
 warnings.filterwarnings('ignore')
-# from __future__ import print_function # In python 2.7
 
 app = Flask(__name__)
-# CORS(app)
+CORS(app)
 
-crop_recommendation_model_path = './XGBoost.pkl'
+crop_recommendation_model_path = './random_forest_model.pkl'
 crop_recommendation_model = pickle.load(
     open(crop_recommendation_model_path, 'rb'))
 
@@ -30,7 +23,6 @@ fertilizer_recommendation_model = pickle.load(
 
 @app.route("/crop", methods=['POST'])
 def members1():
-
     try:
         N = int(request.json['N'])
         P = int(request.json['P'])
@@ -42,11 +34,6 @@ def members1():
         end_month = int(request.json['end_month'])
     except:
         return jsonify({"crop": 'Enter Valid Details', "data": request.json})
-    # return jsonify({"crop": 'printing request', "data": request.json})
-
-    temprature = 20
-    humidity = 30
-    rainfall = 100
 
     x = requests.get('https://api.mapbox.com/geocoding/v5/mapbox.places/' + district + ' ' + state +
                      '.json?access_token=pk.eyJ1Ijoic2FpZ29ydGk4MSIsImEiOiJja3ZqY2M5cmYydXd2MnZwZ2VoZzl1ejNkIn0.CupGYvpb_LNtDgp7b-rZJg')
@@ -57,69 +44,77 @@ def members1():
         coordinates[1]) + '&lon=' + str(coordinates[0]) + '&appid=288a2f93f39e83cd21b86dba6b329fd8')
 
     humidity = y.json()['main']['humidity']
-    temprature = y.json()['main']['temp']
+    temprature = y.json()['main']['temp'] - 273.15
 
-    df = pd.read_csv("./data2.csv")
-    # q = df.query('STATE_UT_NAME=="ANDAMAN And NICOBAR ISLANDS" and DISTRICT == "NICOBAR"', inplace = False)
-    q = df.query('STATE_UT_NAME == "{}" and DISTRICT == "{}"'.format(
-        state, district), inplace=False)
+    df = pd.read_csv("./rainfall.csv")
+    q = df.loc[(df['STATE_UT_NAME'] == state) & (df['DISTRICT'] == district)]
 
     total = 0
-    # l=12
+    l = 12
 
     if start_month <= end_month:
         l = (end_month-start_month)+1
-
         for i in range(start_month, end_month+1):
             try:
                 total += int(q[i:i+1].value)
             except:
                 total -= 1
-
     elif start_month > end_month:
         l = (end_month+12) - start_month + 1
-
         for i in range(start_month, 13):
             try:
                 total += int(q[i:i+1].value)
             except:
                 total -= 1
-
         for i in range(1, end_month+1):
             try:
                 total += int(q[i:i+1].value)
             except:
                 total -= 1
 
-    # for i in range(x, y+1):
-    #     k = i
-    #     if k>12:
-    #         k=k-12
-    #     total+=int(q[k:k+1].value)
-    # for i in range(len(q)):
-    #     total+=int(q[i:i+1].value)
-
-    # l=12
     avg_rainfall = total/l
+    data = np.array([N, P, K, temprature, humidity,
+                    ph, avg_rainfall]).reshape(1, -1)
 
-    data = np.array([[N, P, K, temprature, humidity, ph, avg_rainfall]])
+    scaler = joblib.load("./scalers/all_scaler.gz")
+    x_scaled = scaler.transform(data)
+    my_prediction = crop_recommendation_model.predict(x_scaled)
+    dict_crop = {'rice': 20,
+                 'maize': 11,
+                 'chickpea': 3,
+                 'kidneybeans': 9,
+                 'pigeonpeas': 18,
+                 'mothbeans': 13,
+                 'mungbean': 14,
+                 'blackgram': 2,
+                 'lentil': 10,
+                 'pomegranate': 19,
+                 'banana': 1,
+                 'mango': 12,
+                 'grapes': 7,
+                 'watermelon': 21,
+                 'muskmelon': 15,
+                 'apple': 0,
+                 'orange': 16,
+                 'papaya': 17,
+                 'coconut': 4,
+                 'cotton': 6,
+                 'jute': 8,
+                 'coffee': 5}
 
-    # data = np.array([[104,18, 30, 23.603016, 60.3, 6.7, 140.91]])
-    # data = np.array([[83, 45, 60, 28, 70.3, 7.0, 150.9]])
-    my_prediction = crop_recommendation_model.predict(data)
-    final_prediction = my_prediction[0]
+    for key, value in dict_crop.items():
+        if value == my_prediction[0]:
+            final_prediction = key
 
     return jsonify({"crop": final_prediction, "data": y.json()['main'], 'l': l})
 
 
 @app.route("/fertilizer", methods=['POST'])
 def members2():
-
     try:
         N = int(request.json['N'])
         P = int(request.json['P'])
         K = int(request.json['K'])
-        # ph = float(request.json['Ph'])
         state = request.json['state']
         district = request.json['district']
         moisture = float(request.json['moisture'])
@@ -128,11 +123,9 @@ def members2():
         start_month = int(request.json['start_month'])
         end_month = int(request.json['end_month'])
     except:
-        return jsonify({"crop": 'failed to get info2', "data": request.json})
+        return jsonify({"crop": 'Enter Valid Data', "data": request.json})
 
-    temprature = 20
     humidity = 30
-    rainfall = 100
 
     x = requests.get('https://api.mapbox.com/geocoding/v5/mapbox.places/' + district + ' ' + state +
                      '.json?access_token=pk.eyJ1Ijoic2FpZ29ydGk4MSIsImEiOiJja3ZqY2M5cmYydXd2MnZwZ2VoZzl1ejNkIn0.CupGYvpb_LNtDgp7b-rZJg')
@@ -143,69 +136,43 @@ def members2():
         coordinates[1]) + '&lon=' + str(coordinates[0]) + '&appid=8d51fbf3b5ad7f3cc65ba0ea07220782')
 
     humidity = y.json()['main']['humidity']
-    temprature = y.json()['main']['temp']
 
-    df = pd.read_csv("./data2.csv")
-    q = df.query(
-        'STATE_UT_NAME=="ANDAMAN And NICOBAR ISLANDS" and DISTRICT == "NICOBAR"', inplace=False)
-    # q = df.query('STATE_UT_NAME == "{}" and DISTRICT == "{}"'.format(state, district), inplace = False)
-
-    # total=0
-    # for i in range(len(q)):
-    #     total+=int(q[i:i+1].value)
-    # avg_rainfall = total/len(q)
-
+    df = pd.read_csv("./rainfall.csv")
+    q = df.loc[(df['STATE_UT_NAME'] == state) & (df['DISTRICT'] == district)]
     total = 0
-    # l=12
+    l = 12
 
     if start_month <= end_month:
         l = (end_month-start_month)+1
-
         for i in range(start_month, end_month+1):
             try:
                 total += int(q[i:i+1].value)
             except:
                 total -= 1
-
     elif start_month > end_month:
         l = (end_month+12) - start_month + 1
-
         for i in range(start_month, 13):
             try:
                 total += int(q[i:i+1].value)
             except:
                 total -= 1
-
         for i in range(1, end_month+1):
             try:
                 total += int(q[i:i+1].value)
             except:
                 total -= 1
 
-    # for i in range(x, y+1):
-    #     k = i
-    #     if k>12:
-    #         k=k-12
-    #     total+=int(q[k:k+1].value)
-    # for i in range(len(q)):
-    #     total+=int(q[i:i+1].value)
-
-    # l=12
     avg_rainfall = total/l
 
     data = np.array(
         [[avg_rainfall, humidity, moisture, soil_type, crop_type, N, K, P]])
 
-    # data = np.array([[26, 52, 38, 4, 3,	37,	0, 0 ]])
-
     my_prediction = fertilizer_recommendation_model.predict(data)
     final_prediction = my_prediction[0]
-    # final_prediction = 'dummie'
 
     fertname = {"0": "10-26-26", "1": "14-35-14", "2": "17-17-17",
                 "3": "20-20", "4": "28-28", "5": "DAP", "6": "Urea"}
 
-    # return jsonify({"crop": fertname[final_prediction], "data": data.tolist()})
     return jsonify({"crop": str(fertname[str(final_prediction)]), "data": fertname})
 
 
