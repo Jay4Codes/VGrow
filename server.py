c276c903751d3f9337988aee374b0404daa7b8b5
Jay Jain <jay.jain@neyx.in>
2023-02-12 01:47:14 +0530

    fert model added

--- server.py
+++ server.py
@@ -17,7 +17,7 @@ crop_recommendation_model_path = './RandomForest_Crop.pkl'
 crop_recommendation_model = pickle.load(
     open(crop_recommendation_model_path, 'rb'))
 
-fertilizer_recommendation_model_path = './XGB_Fertilizer.pkl'
+fertilizer_recommendation_model_path = './RandomForest_Fertilizer.pkl'
 fertilizer_recommendation_model = pickle.load(
     open(fertilizer_recommendation_model_path, 'rb'))
 
@@ -125,14 +125,10 @@ def members2():
         state = request.json['state']
         district = request.json['district']
         moisture = float(request.json['moisture'])
-        soil_type = request.json['soil_type']
-        crop_type = request.json['crop_type']
-        start_month = int(request.json['start_month'])
-        end_month = int(request.json['end_month'])
+        soil_type = int(request.json['soil_type'])
+        crop_type = int(request.json['crop_type'])
     except:
         return jsonify({"crop": 'Enter Valid Data', "data": request.json})
-
-    humidity = 30
 
     x = requests.get('https://api.mapbox.com/geocoding/v5/mapbox.places/' + district + ' ' + state +
                      '.json?access_token=pk.eyJ1Ijoic2FpZ29ydGk4MSIsImEiOiJja3ZqY2M5cmYydXd2MnZwZ2VoZzl1ejNkIn0.CupGYvpb_LNtDgp7b-rZJg')
@@ -142,45 +138,27 @@ def members2():
     y = requests.get('https://api.openweathermap.org/data/2.5/weather?lat=' + str(
         coordinates[1]) + '&lon=' + str(coordinates[0]) + '&appid=8d51fbf3b5ad7f3cc65ba0ea07220782')
 
-    humidity = y.json()['main']['humidity']
-
-    df = pd.read_csv("./rainfall.csv")
-    q = df.loc[(df['STATE_UT_NAME'] == state) & (df['DISTRICT'] == district)]
-    total = 0
-    l = 12
-
-    if start_month <= end_month:
-        l = (end_month-start_month)+1
-        for i in range(start_month, end_month+1):
-            try:
-                total += int(q[i:i+1].value)
-            except:
-                total -= 1
-    elif start_month > end_month:
-        l = (end_month+12) - start_month + 1
-        for i in range(start_month, 13):
-            try:
-                total += int(q[i:i+1].value)
-            except:
-                total -= 1
-        for i in range(1, end_month+1):
-            try:
-                total += int(q[i:i+1].value)
-            except:
-                total -= 1
-
-    avg_rainfall = total/l
+    temperature = y.json()['main']['temp'] - 273.15
 
     data = np.array(
-        [[avg_rainfall, humidity, moisture, soil_type, crop_type, N, K, P]])
+        [[temperature, moisture, soil_type, crop_type, N, K, P]]).reshape(1, -1)
 
-    my_prediction = fertilizer_recommendation_model.predict(data)
-    final_prediction = my_prediction[0]
+    my_prediction = fertilizer_recommendation_model.predict(data)[0]
+    print(my_prediction)
 
-    fertname = {"0": "10-26-26", "1": "14-35-14", "2": "17-17-17",
-                "3": "20-20", "4": "28-28", "5": "DAP", "6": "Urea"}
+    dict_fert = {'10-26-26': 0,
+                 '14-35-14': 1,
+                 '17-17-17': 2,
+                 '20-20': 3,
+                 '28-28': 4,
+                 'DAP': 5,
+                 'Urea': 6}
 
-    return jsonify({"crop": str(fertname[str(final_prediction)]), "data": fertname})
+    for key, value in dict_fert.items():
+        if value == my_prediction:
+            final_pred = key
+
+    return jsonify({"crop": final_pred, "data": dict_fert})
 
 
 if __name__ == "__main__":
